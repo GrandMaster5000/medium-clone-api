be281cdc96020340522e0d69d93932e544f0f2bd
Tolik Korobskix <ggamer556@mail.ru>
2022-03-19 14:09:43 +0300

    connect psql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TagsModule } from '@app/tags/tags.module';
+import { ormConfig } from '@app/config/orm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-	imports: [TagsModule],
+	imports: [TypeOrmModule.forRoot(ormConfig), TagsModule],
 })
 export class AppModule {}
