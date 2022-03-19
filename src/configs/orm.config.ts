cdb8443e95996703a8274f7e7236967c889de4a6
Tolik Korobskix <ggamer556@mail.ru>
2022-03-19 14:40:49 +0300

    add tag entity

--- src/configs/orm.config.ts
+++ src/configs/orm.config.ts
@@ -9,5 +9,7 @@ export const getOrmConfig = (configService: ConfigService): ConnectionOptions =>
 		username: configService.get('DATABASE_USERNAME'),
 		password: configService.get('DATABASE_PASSWORD'),
 		database: configService.get('DATABASE_NAME'),
+		entities: [__dirname + '/**/*.entity{.ts,.js}'],
+		synchronize: true,
 	};
 };
